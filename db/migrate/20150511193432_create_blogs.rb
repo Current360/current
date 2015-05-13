ee4968bb1f474a7fe075b63f83c8c9c7bd0cbc2a
abouvier <abouvier@currentmarketing.com>
2015-05-13 15:15:55 -0400

    adding files I forgot on previous commit

---
+++ db/migrate/20150511193432_create_blogs.rb
@@ -0,0 +1,14 @@
+class CreateBlogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+      t.string :title
+      t.text :content
+      t.integer :category
+      t.integer :author
+      t.string :blog_picture
+      t.string :slug
+
+      t.timestamps
+    end
+  end
+end
